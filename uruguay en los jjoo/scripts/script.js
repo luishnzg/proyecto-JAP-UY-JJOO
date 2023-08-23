c485d43e1aa653ff62f79cdc9c0826ea04fc4da7
luishnzg <luismhnzg@gmail.com>
2023-08-22 22:19:31 -0300

    first commit

---
+++ uruguay en los jjoo/scripts/script.js
@@ -0,0 +1,11 @@
+const medallas_url = "https://danikho2020.github.io/json/medallas.json";
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("cargar").addEventListener("click", function () {
+
+        /* 
+            Escribe aquí el código necesario para lograr el comportamiento esperado
+         */
+
+    });
+});
